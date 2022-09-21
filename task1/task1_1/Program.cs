5d36ed1063e822f568c39cc622b524de718609d4
Ilya Posudnevsky <ilyaposudnevsky@gmail.com>
2022-09-21 11:31:59 +0300

    Add task1

---
+++ task1/task1_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter b: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                int temp;
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            Console.Write("Result: ");
+            int amount = 0;
+            for (int i = a; i <= b; i++)
+            {
+                int counter = 0;
+                char[] arr = convertTo3(i).ToCharArray();
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == '2')
+                        counter++;
+                }
+                if (counter == 2)
+                {
+                    Console.Write(i + " ");
+                    amount++;
+                }
+            }
+            if (amount == 0)
+                Console.Write("There are no such numbers");
+        }
+
+        public static string convertTo3(int a)
+        {
+            string s = "";
+            while (a > 0 || a < 0)
+            {
+                int t = a % 3;
+                a /= 3;
+                s += t.ToString();
+            }
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return (new string(arr));
+        }
+    }
+}
